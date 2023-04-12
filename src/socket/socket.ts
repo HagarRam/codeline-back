50ba0e6828b7e3e415afa7025cfaf41830308794
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-04-12 22:55:11 +0300

    with socket

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -3,26 +3,99 @@ import http from 'http';
 import { CodeBlockModal } from '../model/codeBlock.model';
 const server = http.createServer();
 const io = new Server(server);
+
+interface ICodeOnline {
+	code: string;
+	readOnly: boolean;
+	mentor: string | null;
+	connectedPeople: number;
+}
+const codeBlock: ICodeOnline[] = [];
+// import { Server, Namespace } from "socket.io";
+// import { getAllRooms } from "../services/room.service";
+
+// interface Room {
+//   id: string;
+//   code: string;
+//   connected: number;
+//   readOnly: boolean;
+//   firstClient: string | null;
+// }
+
+// let rooms: Room[] = [];
+
+// async function initRooms() {
+//   const allRooms = await getAllRooms();
+//   rooms = allRooms.map((r) => ({
+//     id: r["roomId"].toString(),
+//     code: r["roomCode"],
+//     connected: 0,
+//     readOnly: true,
+//     firstClient: null,
+//   }));
+// }
+
+// export function startSocketServer(server: any) {
+//   const io = new Server(server, {
+//     cors: {
+//       // origin: "http://localhost:3000",
+//       origin: "https://mhomeassigment-front.onrender.com",
+//       methods: ["GET", "POST"],
+//     },
+//   });
+
+//   initRooms();
+// // handling with adding multiple users to the same room and giving them permission to view or edit
+//   io.on("connection", (socket) => {
+
+//     socket.on("joinRoom", (roomId) => {
+//       const room = rooms[Number(roomId) - 1];
+//       socket.join(room.id);
+//       room.connected = room.connected + 1;
+
+//       socket.emit("codeUpdate", room.code);
+//       socket.emit("readOnly", room.readOnly);
+//       if (room.connected === 1) {
+//         room.readOnly = true;
+//         room.firstClient = socket.id;
+//       } else {
+//         room.readOnly = false;
+//       }
+
+//       if (room.connected > 1 && socket.id !== room.firstClient) {
+//         io.to(socket.id).emit("readOnly", false);
+//       }
+//     });
+
+//     socket.on("codeUpdate", (newCode) => {
+//       const room = rooms.find((r) => socket.rooms.has(r.id));
+//       if (room) {
+//         room.code = newCode;
+//         socket.to(room.id).emit("codeUpdate", newCode);
+//       }
+//     });
+
+//     socket.on("disconnect", () => {
+//       const room = rooms.find((r) => r.id == socket.handshake.query.roomId);
+//       if (room) {
+//         if (room.connected > 0) {
+//           room.connected--;
+//         }
+
+//         if (room.connected === 0) {
+//           room.readOnly = true;
+//           io.to(room.id).emit("readOnly", room.readOnly);
+//         }
+//       }
+//     });
+//   });
+// }
+
 io.on('connection', (socket) => {
 	console.log('a user connected');
 	socket.on('code_block', (data) => {
 		console.log(`Received new code block: ${data}`);
-		// const _id = data._id;
-		// const newData = data.data;
-		// if (down) {
-		// 	CodeBlockModal.findByIdAndUpdate(_id, newData, { new: true })
-		// 		.then((updatedCodeBlock) => {
-		// 			console.log('Updated code block:', updatedCodeBlock);
-		// 			io.to('code room').emit('code block', {
-		// 				code: updatedCodeBlock?.code,
-		// 			});
-		// 		})
-		// 		.catch((err) => {
-		// 			console.error('Error updating code block:', err);
-		// 		});
-		// }
 	});
-
 	socket.on('disconnect', () => {
 		console.log('user disconnected');
 	});
@@ -30,3 +103,18 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
 	console.log('Server listening on port 3000.');
 });
+
+// const _id = data._id;
+// const newData = data.data;
+// if (down) {
+// 	CodeBlockModal.findByIdAndUpdate(_id, newData, { new: true })
+// 		.then((updatedCodeBlock) => {
+// 			console.log('Updated code block:', updatedCodeBlock);
+// 			io.to('code room').emit('code block', {
+// 				code: updatedCodeBlock?.code,
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			console.error('Error updating code block:', err);
+// 		});
+// }
